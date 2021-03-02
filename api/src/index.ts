06d1d60e05e3ac7c028ac058ca2ca6d024098c7f
Łukasz Urbański <lukasz.urbanski@student.uj.edu.pl>
2021-03-02 23:07:37 +0100

    Connect mongo and redis to the app

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,18 @@
-import { APP_PORT, APP_ORIGIN } from "./config";
+import mongoose from "mongoose";
+import session from "express-session";
+import Redis from "ioredis";
+import connectRedis from "connect-redis";
+import { REDIS_OPTIONS, APP_PORT, MONGO_URI, MONGO_OPTIONS, APP_ORIGIN } from "./config";
 import { createApp } from "./app";
 
-((): void => {
-    const app = createApp();
+export let redisClient: Redis.Redis;
+
+(async (): Promise<void> => {
+    await mongoose.connect(MONGO_URI, MONGO_OPTIONS);
+    const RedisStore = connectRedis(session);
+    redisClient = new Redis(REDIS_OPTIONS);
+    const store = new RedisStore({ client: redisClient });
+    const app = createApp(store);
     app.listen(APP_PORT, () => {
         // eslint-disable-next-line no-console
         console.log(`Server is up on ${APP_ORIGIN}`);
