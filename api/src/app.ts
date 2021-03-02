205bfedf890f5d4669815e7c68c4c76fa6620031
Łukasz Urbański <lukasz.urbanski@student.uj.edu.pl>
2021-03-02 23:19:08 +0100

    Connect db cache
    Connect mongo and redis to the app, add env reading

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,10 +1,12 @@
 import express, { Express } from "express";
+import session, { Store } from "express-session";
+import { SESSION_OPTIONS } from "./config";
 import { homeRoute } from "./routes";
 
-
-export const createApp = (): Express => {
+export const createApp = (store: Store): Express => {
     const app = express();
     app.use(express.json());
+    app.use(session({ ...SESSION_OPTIONS, store }));
 
     app.use("/", homeRoute);
 
