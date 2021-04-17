e373c588c1e1415ac3d1e3e0c544228f07ba9630
Łukasz Urbański <lukasz.urbanski@student.uj.edu.pl>
2021-04-17 23:02:56 +0200

    Fix

---
+++ .devcontainer/mongo/mongo-init.sh
@@ -0,0 +1,18 @@
+mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+  db.createUser({
+    user: "$MONGO_USERNAME",
+    pwd: "$MONGO_PASSWORD",
+    roles: [
+      { role: 'readWrite', db: "$MONGO_INITDB_DATABASE" }
+    ]
+  })
+  db.users.insertOne({
+    "email" : "test@swapi.com",
+    "name" : "test",
+    "password" : "\$2a\$12\$zLi/jdjju85ff435GheaIugOxf6VEFayReOv3K7/Vw7bVNtdr3pL2",
+    "swCharacterId" : NumberInt(1),
+    "verifiedAt" : new Date(),
+    "createdAt" : new Date(),
+    "updatedAt" : new Date()
+})
+EOF
